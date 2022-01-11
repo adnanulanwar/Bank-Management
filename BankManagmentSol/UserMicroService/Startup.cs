e238287398e7d60cb30f10da668daf656712769f
Rasel <mahfuz.phycu@gmail.com>
2022-01-11 16:54:32 +0600

    Project Upload

--- BankManagmentSol/UserMicroService/Startup.cs
+++ BankManagmentSol/UserMicroService/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserMicroService.Contexts;
+using UserMicroService.Repositories.Implements;
+using UserMicroService.Repositories.Interfaces;
 
 namespace UserMicroService
 {
@@ -26,6 +30,11 @@ namespace UserMicroService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string conn = Configuration.GetConnectionString("conn");
+            services.AddDbContext<UserDbContext>(options => options.UseSqlServer(conn));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
